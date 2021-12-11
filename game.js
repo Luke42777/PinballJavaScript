2017b775976f462e7a3607ba9567f2148d89e966
Luke42777 <55854062+Luke42777@users.noreply.github.com>
2021-12-11 16:06:40 +0000

    class Game done, game works

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ class Game {
         this.stats = new Statistics();
         this.wallet = new Wallet(amount);
         //select html elements needed
-        document.getElementById("start").addEventListener("click", this.startGame);
+        document.getElementById("start").addEventListener("click", this.startGame.bind(this));
         this.spanWallet = document.querySelector("span.wallet");
         this.boards = document.querySelectorAll("div.color");
         this.inputBid = document.getElementById("bid");
@@ -14,29 +14,55 @@ class Game {
 
         this.render();
 
+
+
     }
-    render(colors = ["#2ee", "#2ee", "#2ee"], money = this.wallet.getWalletStatus(), stats = [0, 0, 0], result = false, bid = 0, moneyWon = 0) {
+    render(colors = ["#2ee", "#2ee", "#2ee"], money = this.wallet.getWalletStatus(), stats = [0, 0, 0], result = "", bid = 0, moneyWon = 0) {
         // initial settings
         this.boards.forEach((board, index) => { board.style.backgroundColor = colors[index] });
         this.spanWallet.textContent = money;
         if (result) {
-            result = `You won: ${moneyWon}`;
+            result = `You won £${moneyWon}`;
         }
         else if (!result && result !== "") {
-            result = `You lost: ${bid}`;
+            result = `You lost £${bid}`;
         }
-
-
         this.spanResult.textContent = result;
         this.spanGames.textContent = stats[0];
         this.spanWins.textContent = stats[1];
         this.spanLosses.textContent = stats[2];
-
-
+        this.inputBid.value = ""; // clear input after rendering
     }
 
     startGame() {
-        console.log('gramy');
+        //validating
+        if (this.inputBid.value < 1)
+            return alert("Miminum bid is 1");
+
+        const bid = Math.floor(this.inputBid.value);
+        //checking 
+        if (!this.wallet.checkIfCanPlay(bid))
+            return alert("You haven't got enough sources to place this bid.I'm affraid");
+
+        this.wallet.changeWalletStatus(bid, "-");
+
+        //drawing process, object class Draw needs to be created on  every call of the method
+
+        this.draw = new Draw();
+        const colors = this.draw.getDrawResult();
+
+        //winner checking
+        const win = Result.checkWin(colors);
+
+        const moneyWon = Result.moneyWon(win, bid);
+        this.wallet.changeWalletStatus(moneyWon); // "+" is default
+
+        this.stats.addGameToStats(win, bid);
+
+        //rendering
+
+        this.render(colors, this.wallet.getWalletStatus(), this.stats.showGameStatistics(), win, bid, moneyWon);
+
 
     }
 }
